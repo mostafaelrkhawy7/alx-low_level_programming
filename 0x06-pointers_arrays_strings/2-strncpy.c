fdb273661a293bffae23c70eeb6f229a22185f0d
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-07-26 00:13:32 +0300

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
  * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
+ * dest: input value
+ * src: input value
+ * n: input value
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int y;
-
 	y = 0;
 	while (y < n && src[y] != '\0')
 	{
