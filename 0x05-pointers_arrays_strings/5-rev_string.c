f76bcfcbb4f5ea2c58f04e8390774a4fc19735b6
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-07-26 00:23:00 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * rev_string - Reverses a string
- * @s: Input string
+ * s: Input string
  * Return: String in reverse
  */
-
 void rev_string(char *s)
 {
 	char rev = s[0];
