308235659ab628bff0b6f5ec5cf22018dd89ad95
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-07-31 23:44:02 +0300

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+        int i = 0;
+        int x = 0;
+        
+        for(s[i] != '\0'; i++)
+        {
+                for(accept[x] != '\0'; x++)
+                {
+                    if (accept[x] == '\0')
+                            return (i);
+                }
+        }
+        return (i)
+}
