57c6f589b764be6bd2f734f85b7ef7b4fb8a1db5
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-02 21:32:58 +0300

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,14 @@
  */     
 int _strlen_recursion(char *s)
 {
-    int i = 0;
-    
-    if (*s > '\0')
-    {
-        i++;
-        i += _strlen_recursion(s + 1) +1;
-    }
-    
-    return (i);
+   int i = 0;
+
+	if (*s > '\0')
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+
+	return (i);
+
 }
