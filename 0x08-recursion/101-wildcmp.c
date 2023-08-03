aa1abb0700f1279b0afa5ae5786e1ae9d450e7e1
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-03 05:16:56 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,8 +20,8 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
-	{
+	
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
+	
 	return (0);
 }
