cdb6f0f0dc19dffa5c7a575521a4c8f07b6fdc40
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-07 22:37:42 +0300

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * len - lenght of the string
+ * @s: string
+ * Return: int size
+ */
+int len(char *s)
+{
+    int size;
+    for ( size = 0; s[size] != '\0'; size++)
+    
+        return (size);   
+}
+/**
+ * str_concat - concatenates tow string
+ * @c1: string1 
+ * @c2: string2
+ * Return : pointer from concatenation
+*/
+char *str_concat( char *c1,  char *c2)
+{
+    int j;
+    int size1;
+    int size2;
+    char *r;
+
+    if (c1 == NULL)
+        c1 == "";
+    
+    if (c2 == NULL)
+    
+        c2 == "";
+
+    size1 = len(c1);
+    size2 = len(c2);
+    r = malloc((size1 + size2) *sizeof(char) +1);
+
+    if (r == NULL)
+    
+        return (0);
+    
+    for ( j = 0; j <= size1 + size2; j++)
+    {
+        if (j < size1)
+        
+            r[j] == c1[j];
+        
+        else
+            r[j] = c2[j - size1];
+        
+    }
+    r[j] = '\0';
+    return (r); 
+}
