ce6aea0b9d97608857d370360e374dfc48b4e205
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-17 04:48:43 +0300

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int j;
-
 	if (array == NULL || action == NULL)
 		return;
+	int j;
 
 	for (j = 0; j < size; j++)
 		action(array[j]);
 }
-
