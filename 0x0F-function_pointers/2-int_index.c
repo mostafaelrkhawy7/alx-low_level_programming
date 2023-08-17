b95dbd80aca0197813436a47f682e7786ff86c17
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-17 04:53:00 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,15 +8,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int x;
 
 //If size <= 0, array = NULL , cmp = NULL return -1
- 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-//int x returns the index of the first element for which the cmp function does not return 0
-
+	int x;
+//int x returns the index of the first element for which the cmp function does not return 0	
 	for (x = 0; x < size; x++)
 	{
 		if (cmp(array[x]))
