8bdc4c6950c8198e813afee291a6af0757cc924c
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-25 03:59:40 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,19 +11,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *n;
+	list_t *new;
 	unsigned int length = 0;
 
 	while (str[length])
 		length++;
 
-	n = malloc(sizeof(list_t));
-	if (!n)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	n->str = strdup(str);
-	n->length = length;
-	n->next = (*head);
-	(*head) = n;
+	new->str = strdup(str);
+	new->length = length;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
