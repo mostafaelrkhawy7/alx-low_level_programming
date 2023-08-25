b79e0787a2f4d03b1fc48517a4509257ebc8366b
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-25 04:35:16 +0300

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - frees list
+ * @head: list_t list to free 
  */
 void free_list(list_t *head)
 {
