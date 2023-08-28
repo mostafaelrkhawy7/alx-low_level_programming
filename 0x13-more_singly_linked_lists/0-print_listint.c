e77722c078f3e1bd390037219b1b788647fcad51
mostafaelrkhawy7 <133015521+mostafaelrkhawy7@users.noreply.github.com>
2023-08-28 23:00:54 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t x = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		number++;
 		h = h->next;
+		x++;
 	}
 
-	return (number);
+	return (x);
 }
